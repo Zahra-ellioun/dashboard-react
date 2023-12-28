33869f8564fb63ad71c8276f2c7a3977b0f6a56c
zahra ellioun <zizi.ell75@gmail.com>
2023-12-28 13:14:56 +0330

    make needs for create site

---
+++ src/component/LogIn.jsx
@@ -0,0 +1,5 @@
+const LogIn = () => {
+  return <h2>hello</h2>;
+};
+
+export default LogIn;
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        dashPanel: "#3D3D44",
+        background: "#F0F2F5",
+        bgCard: "#FFFFFF",
+        title: "#344775",
+        text: "#7B82B1",
+        green: "#17C653",
+        red: "#F8285A",
+        blue: "#056EE9",
+      },
+    },
+  },
+  plugins: [],
+};
